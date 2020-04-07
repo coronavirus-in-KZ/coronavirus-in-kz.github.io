563d68486cc756e2a2f5c74f23bd8adef362e6e0
zloy-zhake <jk-jdogg@yandex.ru>
2020-04-07 23:35:07 +0600

    Реализовал генерацию таблицы. Добавил стили.

--- generate_readme.py
+++ generate_readme.py
@@ -2,7 +2,7 @@
 
 # %%
 import pandas as pd
-from yattag import Doc
+from yattag import Doc, indent
 
 # %%
 corona_df = pd.read_excel(io="coronavirus.xlsx", header=0)
@@ -11,40 +11,57 @@ corona_df = pd.read_excel(io="coronavirus.xlsx", header=0)
 doc, tag, text = Doc().tagtext()
 
 # %%
-with tag("table", border=1):
-    headers = list(corona_df)
-    for header in headers:
-        with tag("th"):
-            text(str(header))
+with tag("html"):
+    with tag("head"):
+        doc.stag(
+            "link",
+            rel="stylesheet",
+            type="text/css",
+            href="styles.css",
+            media="screen",
+        )
 
-    rows = corona_df.values.tolist()
-    for row in rows:
-        with tag("tr"):
-            if "зараженные" in row:
-                for cell in row:
-                    if isinstance(cell, str):
-                        with tag("td", align="center"):
-                            text(cell)
-                    elif cell > 0:
-                        with tag("td", align="right", bgcolor="red"):
-                            text(cell)
-                    elif cell == 0:
-                        with tag("td", align="right", bgcolor="green"):
-                            text(cell)
-            else:
-                for cell in row:
-                    if isinstance(cell, str):
-                        with tag("td", align="center"):
-                            text(cell)
-                    elif cell > 0:
-                        with tag("td", align="right"):
-                            text(cell)
-                    elif cell == 0:
-                        with tag("td", align="right"):
-                            text(cell)
+    with tag("body"):
+        # TODO добавить <caption>
+        with tag("table", border="1", klass="big_table"):
+            headers = list(corona_df)
+            for header in headers:
+                with tag("th"):
+                    text(str(header))
 
-with open(file="test.html", mode="w") as f_html:
-    print(doc.getvalue(), file=f_html)
+            rows = corona_df.values.tolist()
+            for row in rows:
+                with tag("tr"):
+                    if "зараженные" in row:
+                        for cell in row:
+                            with tag("td"):
+                                if isinstance(cell, str):
+                                    if cell == "-":
+                                        doc.attr(klass="center")
+                                    else:
+                                        doc.attr(klass="left")
+                                elif cell > 0:
+                                    doc.attr(klass="right red")
+                                elif cell == 0:
+                                    doc.attr(klass="right green")
+                                text(cell)
+                    else:
+                        for cell in row:
+                            with tag("td"):
+                                if isinstance(cell, str):
+                                    if cell == "-":
+                                        doc.attr(klass="center")
+                                    else:
+                                        doc.attr(klass="left")
+                                elif cell > 0:
+                                    doc.attr(klass="right")
+                                elif cell == 0:
+                                    doc.attr(klass="right")
+                                text(cell)
+
+result_html = indent(doc.getvalue())
+with open(file="index.html", mode="w") as f_html:
+    print(result_html, file=f_html)
 
 # %%
 row
